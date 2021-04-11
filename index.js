1ee0073f304218cf8777f241780e4cfacfea4f63
Yonle <63401712+Yonle@users.noreply.github.com>
2021-04-11 15:30:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 const config = {
   muted_roles_name: "Muted", // Muted Roles name.
   track_userbot: true, // `false` to not tracking userbot. `true` to tracks userbot. Default is `true`.
-  mute_userbot: false, // `false` to not mute userbot, `true` to mute userbot. Default is `false`.
-  warn_max_msg: 7, // Warning message limit, Default is 7.
+  mute_userbot: false, // `false` to not mute userbot, `true` to mute userbot Default is `false`.
+  warn_max_msg: 5, // Warning message limit, Default is 6.
   max_msg: 10 // Message limit per user, Default is 10.
 };
 
@@ -28,10 +28,10 @@ const guild = new Map();
 bot.on("message", async (message) => {
 
   // Now look at config back, If the owner didn't allows "this" bot to track last bots, Do nothing.
-  if (message.author.bot) { if (!track_userbot) return; } 
+  if (message.author.bot) { if (!config.track_userbot) return; } 
 
   // Do not track your own bots.
-  if (message.author.id === bot.user.id)
+  if (message.author.id === bot.user.id) return;
   // Get user id
   var user_id = message.author.id;
   // Get member information
@@ -58,14 +58,23 @@ if(message.member.hasPermission('MANAGE_ROLES') || message.member.hasPermission(
 }
   guild.set(guild_id, { user_id: user_id, m_count: m_count+1 });
   console.log(`[INFO] User ${user_id} messaging, Message Count:`, m_count);
-  if (guild.get(guild_id).user_id === config.warn_max_msg) { if (message.author.bot) { if (!config.mute_userbot) { return false; } else { return true; } } message.reply("Your message limit is almost over the message limit per user. If you continue, you may be automatically muted by this bot."); }
-  if (guild.get(guild_id).m_count === config.max_msg) {
+  
+  if (guild.get(guild_id).m_count > config.warn_max_msg) { 
+  	if (message.author.bot) { 
+  		if (!config.mute_userbot) { 
+  	  	return false; 
+  	  }
+  	} else {
+     message.reply("Your message limit is almost over the message limit per user. If you continue, you may be automatically muted by this bot.").catch(console.error); 
+  	}
+  }
+  if (guild.get(guild_id).m_count > config.max_msg) {
         
         // Now look again your config back, If the owner didn't allows "this" bot to mute last bots, Do nothing.
-        if (message.author.bot) { if (!mute_userbot) return; } 
+        if (message.author.bot) { if (!config.mute_userbot) return; } 
 
         // Give a trigger if the bot has no MANAGE_ROLES permission.
-        if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return;
+        if(!message.guild.me.hasPermission("MANAGE_ROLES")) return;
    	let muteRole = message.guild.roles.cache.find(r => r.name === config.muted_roles_name);
 	console.log(`[INFO] Max Message Count in ${guild_id} Reached. Finding Mute roles....`);
         // If there's no Muted roles named `Muted`, Create one.
@@ -80,7 +89,6 @@ if(message.member.hasPermission('MANAGE_ROLES') || message.member.hasPermission(
 				}
 			});
 		} catch (e) {
-			message.channel.send("An error occurted. try again later.")
 			console.log(e.stack);
 		}
 	}
@@ -93,19 +101,19 @@ if(message.member.hasPermission('MANAGE_ROLES') || message.member.hasPermission(
 			"ADD_REACTIONS": false,
 			"SPEAK": false,
 			"STREAM": false
-		});
+		}).catch(console.error);
 	});
    // If it's Admin/Moderator, Don't mute
 if(message.member.hasPermission('MANAGE_ROLES') || message.member.hasPermission(["KICK_MEMBERS", "BAN_MEMBERS"]) || message.guild.user === message.author) return;
    member.roles.add(muteRole.id).then(() => {
      console.log(`[INFO] Muted ${user_id}`);
      message.channel.send("Yeah i don't like you flooding,\n**Muted "+message.author.tag+"!!**");
-   }).catch(console.error)
+   }).catch(console.error);
   }
 });
 	
 bot.on("message", (message) => {
-        if (!message.mentions.members.first()||!message.mentions.members.first().id) return;
+  if (!message.mentions.members.first()||!message.mentions.members.first().id) return;
 	if (message.mentions.members.first().id === bot.user.id) {
 		message.author.send("Hello, This bot has no Prefix. If you want invite this bot, Here is it:\nhttps://discord.com/api/oauth2/authorize?client_id="+bot.user.id+"&permissions=268436480&scope=bot")
 	}
